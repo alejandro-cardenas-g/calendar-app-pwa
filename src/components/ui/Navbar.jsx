a2ecbe1653e6f57081424a2608c64539d078544d
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2022-05-10 22:08:11 -0500

    pwa workbox

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Offline, Online } from 'react-detect-offline';
 import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth';
 import { eventPurge } from '../../actions/calendar';
+
+
 
 export const Navbar = () => {
 
@@ -20,6 +23,17 @@ export const Navbar = () => {
             <span className="navbar-brand">
                 {name}    
             </span>
+
+            <Online>
+                <span className="text-success">
+                    Online
+                </span>
+            </Online>
+            <Offline>
+                <span className="text-danger">
+                    Offline
+                </span>
+            </Offline>
 
             <button className="btn btn-outline-danger"
                 onClick={handleLogout}
